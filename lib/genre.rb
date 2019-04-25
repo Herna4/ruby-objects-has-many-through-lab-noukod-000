7dc525c4aa43d0b4413a9b40dcb1d246530b8990
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-25 20:39:17 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,29 +1,27 @@
-class Genre 
-  
+class Genre
   attr_accessor :name
-  
-  @@all =[] 
-  
-  def initialize(name) 
-    @name = name 
-    @@all << self 
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    self.class.all << self
   end
-  
-  def self.all 
-    @@all 
+
+  def self.all
+    @@all
   end
-  
+
+  def artists
+    songs.map do |song|
+      song.artist
+    end
+  end
+
   def songs
     Song.all.select do |song|
       song.genre == self
     end
-  end 
-  
-  def artists
-    Song.all.collect do |song|
-      song.artist
-    end
   end
-    
-    
+
 end
