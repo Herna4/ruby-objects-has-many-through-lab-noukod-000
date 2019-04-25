5124c9b132d0b6bb41b8ed5af7181528e29a49a1
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-25 20:57:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,3 @@
-# class Song
-#   attr_accessor :name, :artist, :genre
-#   @@all = []
-
-#   def initialize(name, artist, genre)
-#     @name, @artist, @genre = name, artist, genre
-#     self.class.all << self
-#   end
-
-#   def self.all
-#     @@all
-#   end
-
-# end
 class Song 
   
   attr_accessor :name, :artist, :genre
