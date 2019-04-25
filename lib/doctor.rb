e8f98f92551e076819163a42ed286ebfedcf919c
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-25 20:38:44 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,18 +1,16 @@
 class Doctor
-  
-  attr_accessor :name, :appointments, :patient
-  
   @@all=[]
+
+  def self.all
+    @@all
+  end
+
+  attr_accessor :name, :appointments, :patient
 
   def initialize(name)
     @name = name
     @appointments = []
     @@all << self
-  end
-  
-  
-  def self.all
-    @@all
   end
 
   def new_appointment(date, patient)
